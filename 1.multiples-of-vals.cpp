97b2f6601d1d1209708dd105bea578166d9b0334
jothamsl <jothamsl1@gmail.com>
2021-06-05 21:18:30 +0100

    Updated multiples of vals

--- 1.multiples-of-vals.cpp
+++ 1.multiples-of-vals.cpp
@@ -3,6 +3,11 @@
 
 int main(void)
 {
+    // x -> first value
+    // y -> second value
+    // v -> accumulator
+    // f -> Upper Bound
+   
     int x = 3, y = 5, v = 0, f = 1000;
     for (int i = 0; i < f; i++)
         if (i % x == 0 || i % y == 0)
