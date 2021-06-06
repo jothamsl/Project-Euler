0982162c181d3d4fe2703df4f09642fff33126a5
jothamsl <jothamsl1@gmail.com>
2021-06-06 09:01:21 +0100

    modified multiples of values

--- 1.multiples-of-vals.cpp
+++ 1.multiples-of-vals.cpp
@@ -1,17 +1,27 @@
 #include <cstdio>
 #include <iostream>
 
+/* 
+ * PROBLEM:
+ * --------
+ * If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. 
+ * The sum of these multiples is 23.
+ * Find the sum of all the multiples of 3 or 5 below 1000.
+ *
+ */
+
 int main(void)
 {
     // x -> first value
     // y -> second value
-    // v -> accumulator
-    // f -> Upper Bound
-   
-    int x = 3, y = 5, v = 0, f = 1000;
-    for (int i = 0; i < f; i++)
+    int x = 3, y = 5, accumulator = 0, upper_bound = 1000;
+
+    // Loop 'upper_bound' times
+    for (int i = 0; i < upper_bound; i++)
+        // if x or y divided by i has no remainder 
         if (i % x == 0 || i % y == 0)
-            v += i;
-    std::cout << v << std::endl;
+            accumulator += i; // Increment accumulator by i
+
+    std::cout << accumulator << std::endl;
     return 0;
 }
